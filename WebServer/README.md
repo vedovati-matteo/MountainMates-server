c53490cb1da3efcf32c732027f3a5f680b26daf1
Matteo Vedovati <68272450+vedovati-matteo@users.noreply.github.com>
2022-11-19 20:40:59 +0100

    :adhesive_bandage: Small Updatates and WebApp README creation

---
+++ WebServer/README.md
@@ -0,0 +1,5 @@
+# Web Server API
+### Run Docker file
+
+BUILD IMAGE: `$ docker build --tag flask-docker .`
+RUN CONTAINER: `$ docker run -p 5000:5000 flask-docker`
--- WebServer/mountainmates/__init__.py
+++ WebServer/mountainmates/__init__.py
@@ -1,11 +1,11 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify
 
 
 def create_app(test_config=None):
 
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
 
     # a simple page that says hello
     @app.route('/')
