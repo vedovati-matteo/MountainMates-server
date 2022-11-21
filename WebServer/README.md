36751f7d54e5165b069a1a80e2115ba752e6557a
Matteo Vedovati <68272450+vedovati-matteo@users.noreply.github.com>
2022-11-21 21:29:24 +0100

    🩹 Flask Fixed

--- WebServer/README.md
+++ WebServer/README.md
@@ -3,4 +3,6 @@
 
 BUILD IMAGE: `$ docker build --tag flask-docker .`
 
-RUN CONTAINER: `$ docker run -p 5000:5000 flask-docker`
+RUN CONTAINER (FIRST TIME): `$ docker run -p 5000:5000 -v ${PWD}:/WebServer flask-docker`
+
+START CONTAINER: ``$ docker start -a -i `docker ps -q -l` ``
--- WebServer/app/main/__init__.py
+++ WebServer/app/main/__init__.py
@@ -1,17 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
 
 from .config import config_by_name
 
 db = SQLAlchemy()
+migrate = Migrate()
 flask_bcrypt = Bcrypt()
 
 
-def create_app(config_name):
+def create_app(config_name='dev'):
     app = Flask(__name__)
     app.config.from_object(config_by_name[config_name])
     db.init_app(app)
+    migrate.init_app(app, db)
     flask_bcrypt.init_app(app)
 
     return app
