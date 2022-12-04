7c51bdc2fae228fbdfd0634e20149e1be603a388
Marco Vinciguerra <88108002+VinciGit00@users.noreply.github.com>
2022-12-04 16:43:20 +0100

    add model

--- WebServer/app/model/escursione_template.py
+++ WebServer/app/model/escursione_template.py
@@ -1,1 +1,19 @@
 from .. import db
+
+class EcursioneTemplate(db.Model):
+    __tablename__ = "EcursioneTemplate"
+    id_escursione = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50))
+    provincia  = db.Column(db.String(2))
+    partenza = db.Column(db.String(30))
+    mapLink =  db.Column(db.String(2000))
+    dislivello = db.Column(db.Integer)
+    distanza = db.Column(db.Float)
+    tempo_stimato = db.Column(db.String(20)) 
+    altezza_min = db.Column(db.Integer) 
+    altezza_max = db.Column(db.Integer) 
+    difficulty = db.Column(db.Integer) 
+    strumenti_richiesti = db.Column(db.String(500)) 
+    descrizione_percorso =  db.Column(db.String(2000))
+    numero_max =  db.Column(db.Integer) 
+    img = db.Column(db.String(500)) 
