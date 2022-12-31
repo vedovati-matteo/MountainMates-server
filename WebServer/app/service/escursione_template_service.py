7ee8260c1739c0a7300b4f68324cdb6307a14530
Marco Vinciguerra <88108002+VinciGit00@users.noreply.github.com>
2022-12-31 16:08:00 +0100

    created API

--- WebServer/app/service/escursione_template_service.py
+++ WebServer/app/service/escursione_template_service.py
@@ -1,12 +1,49 @@
 from .. import db
 from app.model.escursione_template import EcursioneTemplate
-
+from datetime import datetime
 
 def get_all_escursioni_template(data):
+    return EcursioneTemplate.query.all()
+
+def save_new_escursione_template(data,  id_req):
+    escursione_template = escursione_template.query.filter_by(id_escursione_template=id_req).first()
+    if not escursione_template:
+        new_escursione_template = EcursioneTemplate(
+            id_escursione_template = id_req,
+            nome = data['nome'],
+            provincia = data['provincia'],
+            partenza = data['partenza'],
+            mapLink = data['mapLink'],
+            dislivello = data['dislivello'], 
+            distanza= data['distanza'],
+            tempo_stimato = data['tempo_stimato'],
+            altezza_min = data['altezza_min'],
+            altezza_max = data['altezza_max'],
+            difficulty = data['difficulty'],
+            strumenti_richiesti = data['strumenti_richiesti'],
+            descrizione_percorso = data['descrizione_percorso'],
+            img = data['img'],
+        )
+        save_changes(new_escursione_template)
+        response_object = {
+            'status': 'successo',
+            'message': 'Utente registrato con successo',
+        }
+        return response_object, 200 
+    else:
+        response_object = {
+            'status': 'fallimento',
+            'message': "template dell'escursione già esistente",
+        }
+        return response_object, 409
+
+def get_escursione_template_self(data):
     pass
 
-def save_new_escursione_template(data):
-    pass
+def get_escursione_template(data, id):
+    return EcursioneTemplate.query.filter_by(id_escursione_template=id).first_or_404()
 
-def get_escursione_self_template(data):
-    pass
+
+def save_changes(data):
+    db.session.add(data)
+    db.session.commit()
